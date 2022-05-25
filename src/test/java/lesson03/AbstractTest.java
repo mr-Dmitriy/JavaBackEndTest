1dcbe46929aa54303009721cd9a57bfd0fdbc838
Dmitriy_gb <d.o.prihodchenko@gmail.com>
2022-05-25 15:39:20 +0300

    updated

--- src/test/java/lesson03/AbstractTest.java
+++ src/test/java/lesson03/AbstractTest.java
@@ -1,29 +1,20 @@
 package lesson03;
 
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.given;
-
 import org.junit.jupiter.api.BeforeAll;
-
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-
-
+import java.util.Properties;
 
 
 public abstract class AbstractTest {
 
-    final static java.util.Properties prop = new java.util.Properties();
+    final static Properties prop = new java.util.Properties();
 
     @BeforeAll
     static void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-    }
-
-    static Integer getMaxIndex(JsonPath response) {
-        return new ArrayList<>(response.get("results")).size() - 1;
     }
 
     public static String getUrl() throws IOException {
